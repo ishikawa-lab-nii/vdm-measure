d894e6c1530bb2a58fe8e74204f44b7a170896b7
Fuyuki Ishikawa <f-ishikawa@nii.ac.jp>
2015-03-06 19:38:56 +0900

    - Updated the default grouping

--- VDM-Measure/src/space/fyufyu/vdm/nodecount/DefaultNodeGrouper.java
+++ VDM-Measure/src/space/fyufyu/vdm/nodecount/DefaultNodeGrouper.java
@@ -15,6 +15,26 @@ import space.fyufyu.vdm.util.NodeType;
  */
 public class DefaultNodeGrouper extends NodeGrouper {
 
+	private static String BASIC_EXP = "1. Basic Exp.";
+	private static String LITERAL_EXP =  "2. Literal Exp.";
+	private static String NUMERICOP_EXP = "3. Numeric Operator Exp.";
+	private static String LOGICOP_EXP = "4. Logic Operator Exp.";
+	private static String ABSTRACTDATAOP_EXP = "5. Set/Seq/Map Operator Exp.";
+	private static String DECLARATIVE_EXP = "6. Declarative Exp.";
+	private static String CONDITIONAL_EXP = "7. Conditional Exp.";
+	private static String FUNCTIONAL_EXP = "8. Functional Exp.";
+	private static String LET_EXP = "9. Let/Def Exp.";
+	private static String NOTSPEC_EXP = "10. Not Yet Exp.";
+	private static String UNDEFINED_EXP = "11. Undefined Exp.";
+	
+	private static String BASIC_STM = "1. Basic Stm.";
+	private static String CONDITIONAL_STM = "2. Conditional Stm.";
+	private static String LOOP_STM = "3. Loop Stm.";
+	private static String ERRORHANDING_STM = "4. Error Handling Stm.";
+	private static String LET_STM = "5. Let/Def Stm.";
+	private static String NOTSPEC_STM = "6. Not Yet Stm.";
+	
+	
 	public static Map<NodeType, Map<String, String>> DEFAULT_GROUPING;
 	
 	static HashSet<String> knownNodeClasses;
@@ -34,138 +54,139 @@ public class DefaultNodeGrouper extends NodeGrouper {
 
 		// Expression nodes
 		LinkedHashMap<String, String> DEFAULT_GROUPING_EXP = new LinkedHashMap<String, String>();
-		DEFAULT_GROUPING_EXP.put("AForAllExp", "Quantifier Exp.");
-		DEFAULT_GROUPING_EXP.put("AExistsExp", "Quantifier Exp.");
-		DEFAULT_GROUPING_EXP.put("AExists1Exp", "Quantifier Exp.");
-		DEFAULT_GROUPING_EXP.put("AIotaExp", "Quantifier Exp.");
-
-		DEFAULT_GROUPING_EXP.put("ALetBeStExp", "Let Be Exp.");
+		DEFAULT_GROUPING_EXP.put("AEqualsBinaryExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("ANotEqualBinaryExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("ANewExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AIsExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AVariableExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AApplyExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AFieldExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AFieldNumberExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("AMkTypeExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("ATupleExp", BASIC_EXP);
+		DEFAULT_GROUPING_EXP.put("ASelfExp", BASIC_EXP);
 		
-		DEFAULT_GROUPING_EXP.put("AEquivalentBooleanBinaryExp",
-				"Logic Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AImpliesBooleanBinaryExp", "Logic Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AAndBooleanBinaryExp", "Logic Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AOrBooleanBinaryExp", "Logic Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ANotUnaryExp", "Logic Operator Exp.");
-
-		DEFAULT_GROUPING_EXP.put("AEqualsBinaryExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("ANotEqualBinaryExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("ANewExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AIsExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AVariableExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AApplyExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AFieldExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AFieldNumberExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("AMkTypeExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("ATupleExp", "Basic Exp.");
-		DEFAULT_GROUPING_EXP.put("ASelfExp", "Basic Exp.");
-
-		DEFAULT_GROUPING_EXP.put("ASetCompSetExp", "Set/Seq/Map Intension Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapCompMapExp", "Set/Seq/Map Intension Exp.");
-
-		DEFAULT_GROUPING_EXP.put("ACardinalityUnaryExp",
-				"Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASetUnionBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASetIntersectBinaryExp",
-				"Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASetDifferenceBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASubsetBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AInSetBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ANotInSetBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ADistUnionUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP
-				.put("ASeqConcatBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AElementsUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASeqCompSeqExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AIndicesUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ASubseqExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ALenUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AMuExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AReverseUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ADistConcatUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AHeadUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ATailUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapDomainUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapUnionBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("APlusPlusBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapletExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapRangeUnaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ADomainResToBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ADomainResByBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ARangeResToBinaryExp", "Set/Seq/Map Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ARangeResByBinaryExp", "Set/Seq/Map Operator Exp.");
+		DEFAULT_GROUPING_EXP.put("AIntLiteralExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ARealLiteralExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("AStringLiteralExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("AQuoteLiteralExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ANilExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ABooleanConstExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ACharLiteralExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ASetEnumSetExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ASetRangeSetExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ASeqEnumSeqExp", LITERAL_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapEnumMapExp", LITERAL_EXP);
 
 		DEFAULT_GROUPING_EXP.put("AGreaterNumericBinaryExp",
-				"Numeric Operator Exp.");
+				NUMERICOP_EXP);
 		DEFAULT_GROUPING_EXP.put("AGreaterEqualNumericBinaryExp",
-				"Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ALessNumericBinaryExp", "Numeric Operator Exp.");
+				NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ALessNumericBinaryExp", NUMERICOP_EXP);
 		DEFAULT_GROUPING_EXP.put("ALessEqualNumericBinaryExp",
-				"Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("APlusNumericBinaryExp", "Numeric Operator Exp.");
+				NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("APlusNumericBinaryExp", NUMERICOP_EXP);
 		DEFAULT_GROUPING_EXP.put("ASubtractNumericBinaryExp",
-				"Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ATimesNumericBinaryExp", "Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("ADivNumericBinaryExp", "Numeric Operator Exp.");
+				NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ATimesNumericBinaryExp", NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ADivNumericBinaryExp", NUMERICOP_EXP);
 		DEFAULT_GROUPING_EXP
-				.put("ADivideNumericBinaryExp", "Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AModNumericBinaryExp", "Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AUnaryMinusUnaryExp", "Numeric Operator Exp.");
-		DEFAULT_GROUPING_EXP.put("AStarStarBinaryExp", "Numeric Operator Exp.");
+				.put("ADivideNumericBinaryExp", NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AModNumericBinaryExp", NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AUnaryMinusUnaryExp", NUMERICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AStarStarBinaryExp", NUMERICOP_EXP);
 
-		DEFAULT_GROUPING_EXP.put("AIntLiteralExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ARealLiteralExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("AStringLiteralExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("AQuoteLiteralExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ANilExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ABooleanConstExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ACharLiteralExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ASetEnumSetExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ASetRangeSetExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("ASeqEnumSeqExp", "Literal Exp.");
-		DEFAULT_GROUPING_EXP.put("AMapEnumMapExp", "Literal Exp.");
+		DEFAULT_GROUPING_EXP.put("AEquivalentBooleanBinaryExp",
+				LOGICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AImpliesBooleanBinaryExp", LOGICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AAndBooleanBinaryExp", LOGICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AOrBooleanBinaryExp", LOGICOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ANotUnaryExp", LOGICOP_EXP);
+		
+		DEFAULT_GROUPING_EXP.put("ACardinalityUnaryExp",
+				ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASetUnionBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASetIntersectBinaryExp",
+				ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASetDifferenceBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASubsetBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AInSetBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ANotInSetBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ADistUnionUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP
+				.put("ASeqConcatBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AElementsUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASeqCompSeqExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AIndicesUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ASubseqExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ALenUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AMuExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AReverseUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ADistConcatUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AHeadUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ATailUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapDomainUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapUnionBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("APlusPlusBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapletExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapRangeUnaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ADomainResToBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ADomainResByBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ARangeResToBinaryExp", ABSTRACTDATAOP_EXP);
+		DEFAULT_GROUPING_EXP.put("ARangeResByBinaryExp", ABSTRACTDATAOP_EXP);
 
-		DEFAULT_GROUPING_EXP.put("AIfExp", "If Exp.");
-		DEFAULT_GROUPING_EXP.put("AElseIfExp", "If Exp.");
-		DEFAULT_GROUPING_EXP.put("ACasesExp", "Case Exp.");
+		DEFAULT_GROUPING_EXP.put("ASetCompSetExp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("AMapCompMapExp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("AForAllExp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("AExistsExp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("AExists1Exp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("AIotaExp", DECLARATIVE_EXP);
+		DEFAULT_GROUPING_EXP.put("ALetBeStExp", DECLARATIVE_EXP);
+
+		DEFAULT_GROUPING_EXP.put("AIfExp", CONDITIONAL_EXP);
+		DEFAULT_GROUPING_EXP.put("AElseIfExp", CONDITIONAL_EXP);
+		DEFAULT_GROUPING_EXP.put("ACasesExp", CONDITIONAL_EXP);
 		
-		DEFAULT_GROUPING_EXP.put("ALambdaExp", "Lambda Exp.");
-		DEFAULT_GROUPING_EXP.put("AFuncInstatiationExp", "Function Type Instantiation Exp.");
-		
-		DEFAULT_GROUPING_EXP.put("AUndefinedExp", "Undefined Exp.");
+		DEFAULT_GROUPING_EXP.put("ALambdaExp", FUNCTIONAL_EXP);
+		DEFAULT_GROUPING_EXP.put("AFuncInstatiationExp", FUNCTIONAL_EXP);
+
+		DEFAULT_GROUPING_EXP.put("ALetDefExp", LET_EXP);
+		DEFAULT_GROUPING_EXP.put("ADefExp", LET_EXP);
+
+		DEFAULT_GROUPING_EXP.put("ANotYetSpecifiedExp", NOTSPEC_EXP);
+		DEFAULT_GROUPING_EXP.put("ASubclassResponsibilityExp", NOTSPEC_EXP);
+
+		DEFAULT_GROUPING_EXP.put("AUndefinedExp", UNDEFINED_EXP);
 		
 		// Statement nodes
 		LinkedHashMap<String, String> DEFAULT_GROUPING_STM = new LinkedHashMap<String, String>();
 
-		DEFAULT_GROUPING_STM.put("AReturnStm", "Atomic Stm.");
-		DEFAULT_GROUPING_STM.put("AAssignmentStm", "Assignment Stm.");
-		DEFAULT_GROUPING_STM.put("ABlockSimpleBlockStm", "Block Stm.");
-		DEFAULT_GROUPING_STM.put("ACallObjectStm", "Object Call Stm.");
-		DEFAULT_GROUPING_STM.put("ACallStm", "Call Stm.");
+		DEFAULT_GROUPING_STM.put("AReturnStm", BASIC_STM);
+		DEFAULT_GROUPING_STM.put("AAssignmentStm", BASIC_STM);
+		DEFAULT_GROUPING_STM.put("ABlockSimpleBlockStm", BASIC_STM);
+		DEFAULT_GROUPING_STM.put("ACallObjectStm", BASIC_STM);
+		DEFAULT_GROUPING_STM.put("ACallStm", BASIC_STM);
 		
-		DEFAULT_GROUPING_STM.put("AIfStm", "If Stm.");
-		DEFAULT_GROUPING_STM.put("AElseIfStm", "If Stm.");
-		DEFAULT_GROUPING_STM.put("ACasesStm", "Case Stm.");
-		DEFAULT_GROUPING_STM.put("AForIndexStm", "For Stm.");
-		DEFAULT_GROUPING_STM.put("AForAllStm", "For Stm.");
-		DEFAULT_GROUPING_STM.put("AForPatternBindStm", "For Stm.");
-		DEFAULT_GROUPING_STM.put("AWhileStm", "While Stm.");
-		DEFAULT_GROUPING_STM.put("ATrapStm", "Tixe Stm.");
-		DEFAULT_GROUPING_STM.put("ATixeStm", "Tixe Stm.");
-
-		DEFAULT_GROUPING_STM.put("ALetDefExp", "Let/Def Exp.");
-		DEFAULT_GROUPING_STM.put("ADefExp", "Let/Def Exp.");
-		DEFAULT_GROUPING_STM.put("ALetStm", "Let/Def Stm.");
-
-		DEFAULT_GROUPING_STM.put("ANotYetSpecifiedExp", "Not Yet Exp.");
-		DEFAULT_GROUPING_STM.put("ANotYetSpecifiedStm", "Not Yet Stm.");
-		DEFAULT_GROUPING_STM.put("ASubclassResponsibilityExp", "Subclass Responsibility Exp.");
-		DEFAULT_GROUPING_STM.put("ASubclassResponsibilityStm", "Subclass Responsibility Stm.");
-		DEFAULT_GROUPING_STM.put("ASkipStm", "Skip Stm.");
-
-		DEFAULT_GROUPING_STM.put("AExitStm", "Exit Stm.");
-		DEFAULT_GROUPING_STM.put("AErrorStm", "Error Stm.");
+		DEFAULT_GROUPING_STM.put("AIfStm", CONDITIONAL_STM);
+		DEFAULT_GROUPING_STM.put("AElseIfStm", CONDITIONAL_STM);
+		DEFAULT_GROUPING_STM.put("ACasesStm", CONDITIONAL_STM);
 		
+		DEFAULT_GROUPING_STM.put("AForIndexStm", LOOP_STM);
+		DEFAULT_GROUPING_STM.put("AForAllStm", LOOP_STM);
+		DEFAULT_GROUPING_STM.put("AForPatternBindStm",LOOP_STM);
+		DEFAULT_GROUPING_STM.put("AWhileStm", LOOP_STM);
+		
+		DEFAULT_GROUPING_STM.put("AExitStm", ERRORHANDING_STM);
+		DEFAULT_GROUPING_STM.put("AErrorStm", ERRORHANDING_STM);		
+		DEFAULT_GROUPING_STM.put("ATrapStm", ERRORHANDING_STM);
+		DEFAULT_GROUPING_STM.put("ATixeStm", ERRORHANDING_STM);
+
+		DEFAULT_GROUPING_STM.put("ALetStm", LET_STM);
+
+		DEFAULT_GROUPING_STM.put("ANotYetSpecifiedStm", NOTSPEC_STM);
+		DEFAULT_GROUPING_STM.put("ASubclassResponsibilityStm", NOTSPEC_STM);
+		DEFAULT_GROUPING_STM.put("ASkipStm", NOTSPEC_STM);
+
 		DEFAULT_GROUPING = new HashMap<NodeType, Map<String, String>>();
 		DEFAULT_GROUPING.put(NodeType.DEF, DEFAULT_GROUPING_DEF);
 		DEFAULT_GROUPING.put(NodeType.EXP, DEFAULT_GROUPING_EXP);
